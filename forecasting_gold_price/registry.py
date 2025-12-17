89a05bd70c82c53b184094f156b72c28f0384d43
sebastien gaillet <sebastiengaillet@hotmail.fr>
2025-12-17 12:15:01 +0100

    load_model mis à jour

--- forecasting_gold_price/registry.py
+++ forecasting_gold_price/registry.py
@@ -2,8 +2,6 @@ from forecasting_gold_price.params import *
 from datetime import datetime
 import joblib
 import pickle
-import glob
-import os
 
 def save_model(model_fit):
     # Save model locally
@@ -19,16 +17,9 @@ def load_model() :
     Return a saved model:
     """
     if MODEL_TARGET == "local":
-        local_model_directory = os.path.join(LOCAL_REGISTRY_PATH, 'models')
-        local_model_paths = glob.glob(f"{local_model_directory}/*.pkl")
-
-        if not local_model_paths:
-            return None
-
-        most_recent_model_path_on_disk = sorted(local_model_paths)[-1]
         #load
-        with open(most_recent_model_path_on_disk, 'rb') as f:
-            latest_model = pickle.load(f)
+        with open('models/20251216_153712_champion_v1.pkl', 'rb') as f:
+            latest_model = joblib.load(f)
 
         print("✅ Model loaded from local disk")
 
